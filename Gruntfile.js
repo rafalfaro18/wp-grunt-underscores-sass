b22f7bfd17a9363055e0e373d7b6b61caa52b7d9
Rafael Alfaro <rafaela@thehangar.cr>
2017-11-21 10:45:24 -0600

    fixes files paths

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,7 +10,7 @@ module.exports = function (grunt) {
           sourcemap: 'none'
         },
         files: {
-          'compiled/style.css' : 'sass/style.scss'
+          './style-human.css' : 'sass/style.scss'
         }
       },
       dist: {
@@ -19,7 +19,7 @@ module.exports = function (grunt) {
           sourcemap: 'none'
         },
         files: {
-          'compiled/style-min.css' : 'sass/style.scss'
+          './style.css' : 'sass/style.scss'
         }
       }
     },
