a58f1a45540a060f20c4318730c073ea43c5925c
Rafael Alfaro <rafaela@thehangar.cr>
2017-11-21 10:05:22 -0600

    adds task watch and default

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,16 @@
 module.exports = function (grunt) {
   grunt.initConfig({
-    pkg : grunt.file.readJSON('package.json')
+    pkg : grunt.file.readJSON('package.json'),
+    /*Watch task*/
+    watch: {
+      css: {
+        files: '**/*.scss',
+        tasks: ['sass']
+      }
+    }
   });
+
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.registerTask('default', ['watch']);
+
 };
